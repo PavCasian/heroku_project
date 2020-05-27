575e133e1f94edd4a8f304e009a28a48d84f48d6
PavCasian <pcasian@spartaglobal.com>
2020-05-27 13:44:44 +0100

    connecting to postgres

--- shareCode/mysqlLink.php
+++ shareCode/mysqlLink.php
@@ -1,19 +1,21 @@
 <?php
-define ('DB_HOST', 'mysql.example.com'); //need actual server URL
-define ('DB_USER', 'myUsername');  // need actual username
-define ('DB_PASSWORD', 'myPassword'); // need actual password
-define ('DB_NAME', 'myDB'); //need actual database name
- $link = @mysqli_connect('localhost', 'root', null);
+
+
  function connectDB($showMsg=false, $useDB=true) {
-	 $currDB = ($useDB) ? DB_NAME : null;
-	 $link = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, $currDB);
-     if($showMsg) { echoMsg($link,'Remote');}
+	 $db = parse_url(getenv("postgres://laigntiobbmepw:8b077c3cc5ad00b5fc84a68727adecb4436d5b18e1491d3cf1f06e579ca8af13"+
+                       "@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dfettkucil99ap"));
+
+	$pdo = new PDO("pgsql:" . sprintf(
+		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
+	));
+    if($showMsg) { echoMsg($link,'Remote');}
      
-	 if(!$link) { //if Failed to connect to the remote server
-	    $link = @mysqli_connect('localhost', 'root', null, $currDB);
-    	if($showMsg)  { echoMsg($link,'Local');}
-	 }
-	return $link;
+	return $pdo;
  }
  
  function echoMsg($Lk, $Txt) {
